39704aa025e22d37905e8830ccb59ee38ebb879d
mike-mlmedia <92647130+mike-mlmedia@users.noreply.github.com>
2025-08-02 15:17:00 -0400

    Align React frontend styling with 11ty theme

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -8,17 +8,17 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<div className="font-sans text-gray-600 dark:text-gray-300">
-			<Navbar />
-			<Hero />
-			<Services />
-			<Portfolio />
-			<Team />
-			<Contact />
-			<Footer />
-		</div>
-	);
+        return (
+                <>
+                        <Navbar />
+                        <Hero />
+                        <Services />
+                        <Portfolio />
+                        <Team />
+                        <Contact />
+                        <Footer />
+                </>
+        );
 }
 
 export default App;
