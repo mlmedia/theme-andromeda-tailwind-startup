89424b5afcff799e3aa86e2cfdb88df0e25dd76c
mike-mlmedia <92647130+mike-mlmedia@users.noreply.github.com>
2025-08-03 02:39:17 -0400

    build: organize output assets

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		open: true,
-	},
+        plugins: [
+                react(),
+                viteStaticCopy({
+                        targets: [
+                                { src: "src/assets/fonts", dest: "assets" },
+                                { src: "src/assets/icons", dest: "assets" },
+                                { src: "src/assets/images", dest: "assets" },
+                        ],
+                }),
+        ],
+        server: {
+                open: true,
+        },
+        build: {
+                rollupOptions: {
+                        output: {
+                                assetFileNames: (assetInfo) => {
+                                        const ext = path
+                                                .extname(assetInfo.name || "")
+                                                .toLowerCase();
+                                        if (ext === ".css") {
+                                                return "css/[name]-[hash][extname]";
+                                        }
+                                        return "assets/[name]-[hash][extname]";
+                                },
+                                chunkFileNames: "js/[name]-[hash].js",
+                                entryFileNames: "js/[name]-[hash].js",
+                        },
+                },
+        },
 });
